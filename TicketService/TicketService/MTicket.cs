c201da9e4ef14ec87db62148c69abd4032a757fe
Alvaro <alvaro@eliminalia.com>
2019-05-22 12:50:26 +0200

    Modelo de datos.

---
+++ TicketService/TicketService/MTicket.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicketService
+{
+    class MTicket
+    {
+        public string _id { get; set; }
+        public DateTime fecha { get; set; }
+        public bool visible { get; set; }
+        public string employeeServiceID { get; set; }
+        public string documentServiceID { get; set; }
+        public string mensaje { get; set; }
+        public string serviceID { get; set; }
+        public string tipoServicio { get; set; }
+    }
+}
