8b8abfc265fc1744aa5ebccd191913ff2b4cc3e1
Alvaro <alvaro@eliminalia.com>
2019-05-22 13:07:05 +0200

    Implementacion de rutas y comandos console.

---
+++ TicketService/TicketService/Controller/TicketController.cs
@@ -0,0 +1,32 @@
+﻿using Grapevine.Interfaces.Server;
+using Grapevine.Server;
+using Grapevine.Server.Attributes;
+using Grapevine.Shared;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicketService
+{
+    [RestResource]
+    class TicketController
+    {
+
+        #region POST
+
+        [RestRoute(HttpMethod = HttpMethod.POST, PathInfo = "/api/ticket/add")]
+        public IHttpContext AddContacto(IHttpContext context)
+        {
+
+            string json = JsonConvert.SerializeObject("", Formatting.Indented);
+            context.Response.AppendHeader("Content-Type", "application/json");
+            context.Response.SendResponse(json);
+            return context;
+        }
+
+        #endregion
+    }
+}
--- TicketService/TicketService/Program.cs
+++ TicketService/TicketService/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Grapevine.Server;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,59 @@ namespace TicketService
     {
         static void Main(string[] args)
         {
+
+            {
+                var v = "v0.1.0.0";
+                var nameService = "Ticket Service";
+                Console.Title = nameService + " " + v;
+                Console.WriteLine("     T I C K E T   S E R V I C E    " + v);
+                var serverStandar = new RestServer();
+
+                var input = "";
+                while ((input = Console.ReadLine()) != "q")
+                {
+                    switch (input)
+                    {
+                        case "start":
+                            Console.WriteLine("Starting service...");
+                            serverStandar.Port = "5000";
+                            serverStandar.Host = "*";
+                            serverStandar.Start();
+                            Console.Title = "[ON]  " + nameService + " " + v;
+
+                            break;
+
+
+                        case "start --log":
+                            Console.WriteLine("Starting service...");
+
+                            using (var server = new RestServer())
+                            {
+                                server.Port = "5000";
+                                server.Host = "*";
+                                server.LogToConsole().Start();
+                                Console.Title = "[ON]  " + nameService + " " + v;
+                                Console.ReadLine();
+                                server.Stop();
+                            }
+
+                            break;
+                        case "stop":
+                            Console.WriteLine("Stopping service...");
+                            serverStandar.Stop();
+                            Console.Title = nameService + " " + v;
+                            break;
+                        case "--version":
+                            Console.WriteLine(v);
+
+                            break;
+                        default:
+                            Console.WriteLine(String.Format("Unknown command: {0}", input));
+                            break;
+                    }
+
+                }
+            }
         }
     }
 }
